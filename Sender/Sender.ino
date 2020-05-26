ae5039c801a31292338b09015a5525829d02de0a
RRothfeld <raoul.rothfeld+github@gmail.com>
2020-05-26 12:21:12 +0200

    Update Sender.ino

--- Sender/Sender.ino
+++ Sender/Sender.ino
@@ -2,30 +2,23 @@
 
 RCSwitch Sender = RCSwitch();
 
-long SerialData = 0;
-long SendeCode = 0;
+int SerialData = 0;
+int SendeCode = 0;
 
 void setup() {
-
   Serial.begin(9600);
   Serial.setTimeout(25);
-  Sender.enableTransmit(9); // Transmitter is connected to Arduino Pin #9 
-  // mySwitch.setProtocol(2);
-  // mySwitch.setPulseLength(320);
-  // mySwitch.setRepeatTransmit(15);
-  
+  Sender.enableTransmit(9); // Transmitter is connected to Arduino Pin #9
+  Serial.println("Sender initialized...");
 }
 
-void loop() {  
-  /* Same switch as above, but using decimal code */
-  long tempSerialData = Serial.parseInt();
+void loop() {
+  int tempSerialData = Serial.parseInt();
   if (tempSerialData > 0) {
     SerialData = tempSerialData;
   }
-  
-  Serial.print("Empfangen: "); Serial.println(SerialData);
+
+  Serial.print("Sending: "); Serial.println(SerialData);
   Sender.send(SerialData, 32);
-  
-  Serial.print("Sende: "); Serial.println(SerialData);
   delay(100);
 }
