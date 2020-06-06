9937d19ea27aecf1a0d5f48abce04f5091201b13
Sepp-Arsch <65899394+Sepp-Arsch@users.noreply.github.com>
2020-06-06 21:57:44 +0200

    LoRa update

--- Sender/Sender.ino
+++ Sender/Sender.ino
@@ -1,11 +1,41 @@
-#include <SPI.h>
-#include <LoRa.h>
+#include <RCSwitch.h>
 
+RCSwitch Sender = RCSwitch();
+
+<<<<<<< Updated upstream
 long SerialData = 0;
+long SendeCode = 0;
+
+void setup() {
+
+  Serial.begin(9600);
+  Serial.setTimeout(25);
+  Sender.enableTransmit(9); // Transmitter is connected to Arduino Pin #9 
+  // mySwitch.setProtocol(2);
+  // mySwitch.setPulseLength(320);
+  // mySwitch.setRepeatTransmit(15);
+  
+}
+
+void loop() {  
+  /* Same switch as above, but using decimal code */
+  long tempSerialData = Serial.parseInt();
+  if (tempSerialData > 0) {
+    SerialData = tempSerialData;
+  }
+  
+  Serial.print("Empfangen: "); Serial.println(SerialData);
+  Sender.send(SerialData, 32);
+  
+  Serial.print("Sende: "); Serial.println(SerialData);
+  delay(100);
+=======
+unsigned long SerialData = 0;
 
 void setup() {
   Serial.begin(9600);
-  while (!Serial);
+  LoRa.begin(433E6);
+  //while (!Serial);
 
   Serial.println("LoRa Sender");
   if (!LoRa.begin(433E6)) {
@@ -13,11 +43,24 @@ void setup() {
     while (1);
   }
   
-  Serial.setTimeout(25);
+  Serial.setTimeout(100);
+
+  while (true) {
+    Serial.println("ROT");
+    LoRa.beginPacket();
+    LoRa.print(199000099);
+    LoRa.endPacket();
+    delay(1000);
+    Serial.println("BLAU");
+    LoRa.beginPacket();
+    LoRa.print(100990099);
+    LoRa.endPacket();
+    delay(1000);
+  }
 }
 
 void loop() {
-  long tempSerialData = Serial.parseInt();
+  unsigned long tempSerialData = Serial.parseInt();
   if (tempSerialData > 0) {
     SerialData = tempSerialData;
   }
@@ -27,5 +70,6 @@ void loop() {
   LoRa.print(SerialData);
   LoRa.endPacket();
 
-  delay(100);
+  delay(1000);
+>>>>>>> Stashed changes
 }
