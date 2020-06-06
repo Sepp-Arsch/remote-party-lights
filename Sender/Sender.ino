13d5ed41afe5fa9436853e358c020df76288ddbe
Raoul <RRothfeld@users.noreply.github.com>
2020-06-06 22:00:34 +0200

    Update Sender.ino

--- Sender/Sender.ino
+++ Sender/Sender.ino
@@ -2,34 +2,6 @@
 
 RCSwitch Sender = RCSwitch();
 
-<<<<<<< Updated upstream
-long SerialData = 0;
-long SendeCode = 0;
-
-void setup() {
-
-  Serial.begin(9600);
-  Serial.setTimeout(25);
-  Sender.enableTransmit(9); // Transmitter is connected to Arduino Pin #9 
-  // mySwitch.setProtocol(2);
-  // mySwitch.setPulseLength(320);
-  // mySwitch.setRepeatTransmit(15);
-  
-}
-
-void loop() {  
-  /* Same switch as above, but using decimal code */
-  long tempSerialData = Serial.parseInt();
-  if (tempSerialData > 0) {
-    SerialData = tempSerialData;
-  }
-  
-  Serial.print("Empfangen: "); Serial.println(SerialData);
-  Sender.send(SerialData, 32);
-  
-  Serial.print("Sende: "); Serial.println(SerialData);
-  delay(100);
-=======
 unsigned long SerialData = 0;
 
 void setup() {
@@ -71,5 +43,4 @@ void loop() {
   LoRa.endPacket();
 
   delay(1000);
->>>>>>> Stashed changes
 }
