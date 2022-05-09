3c61c4b1b318df31102ae41bcaade79ed084e55c
Raoul <RRothfeld@users.noreply.github.com>
2022-05-09 22:18:37 +0200

    SerialPort connect fails with larger databits settings

--- WebServer/src/webserver/data/SerialSettings.java
+++ WebServer/src/webserver/data/SerialSettings.java
@@ -9,7 +9,7 @@ public class SerialSettings {
     public final boolean onlySendOnChanges;
 
     public SerialSettings() {
-            this(200, 9600, 22, 1, 0, true);
+            this(200, 9600, 8, 1, 0, true);
     }
 
     public SerialSettings(int delay, int baudRate, int dataBits, int stopBits, int parity, boolean onlySendOnChanges) {
